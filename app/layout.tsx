b0524970ef59bece42820e9e6ee4c0dd062a2640
zhuharvey <zhuharvey@gmail.com>
2026-01-17 22:55:32 -0500

    landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        suppressHydrationWarning={true}
         className={`${geistSans.variable} ${geistMono.variable} ${playfair.variable} antialiased`}
       >
         {children}
